54876678db4266273e0d096f2e485d0d63db17ae
Thayghethy Souza dos Santos <101431239+Thayghethy@users.noreply.github.com>
2022-03-30 19:18:57 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+console.log('Vai começar...')
+for(var c =1;c <= 5;c++ ) {
+    console.log(`Valor ${c}`)
+}
+console.log('FIM')
+
+
+
+/*var c = 1
+do {
+    console.log(`Valor ${c}`)
+    c ++
+} while (c <= 5)*/
+
+
+
+/*var c = 1
+while (c <= 5) {
+    console.log(`valor ${c}`)
+    c++
+}*/
